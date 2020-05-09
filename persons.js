836daf66daca8f4ef5bf2f8deab50e25f0a02be6
edson <edson_lopez@hotmail.com.ar>
2020-05-09 18:18:56 -0400

    edson changes

---
+++ persons.js
@@ -0,0 +1,6 @@
+
+var person = [
+    {name:"edson", lastname:"lopez", age: 32, birtday:"28/11/2020"},
+    {name:"Juan", lastname:"Perez", age: 32, birtday:"28/11/2020"},
+    {name:"Luis", lastname:"Ramires", age: 32, birtday:"28/11/2020"}
+]
