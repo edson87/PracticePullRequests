b09e6e9b205fbca40759d4430cd35eb1ffa196a0
edson <edson_lopez@hotmail.com.ar>
2020-05-09 18:32:42 -0400

    x changes

--- persons.js
+++ persons.js
@@ -5,5 +5,6 @@ var person = [
     {name:"Luis", lastname:"Ramires", age: 32, birtday:"28/11/2020"},
     {name:"Luis", lastname:"Ramires", age: 32, birtday:"28/11/2020"},
     {name:"paco", lastname:"Gonzales", age: 32, birtday:"28/11/2020"},
+    {name:"pedro", lastname:"Salvatierra", age: 32, birtday:"28/11/2020"},
     {name:"pedro", lastname:"Salvatierra", age: 32, birtday:"28/11/2020"}
-]
+];
